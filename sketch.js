27c22b9c4d45e820c3d83ff131cd3d9dd9a6f407
AafreenKhan040896 <77886222+AafreenKhan040896@users.noreply.github.com>
2022-05-28 15:35:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+
+function preload(){
+}
+
+function setup() {
+  createCanvas(600,700);
+  
+  engine = Engine.create();
+  world = engine.world;
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+}
+
+function draw() {
+  background(51);
+}
+
